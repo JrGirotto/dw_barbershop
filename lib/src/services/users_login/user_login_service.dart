b8b2423f50e4219ffccefb2cd8e7013ea1ce0250
jrgirotto <jrgirotto@live.com>
2023-09-20 14:42:43 -0300

    Implementando Riverpod - login ok

---
+++ lib/src/services/users_login/user_login_service.dart
@@ -0,0 +1,9 @@
+import 'package:dw_barbershop/src/core/exceptions/service_exception.dart';
+import 'package:dw_barbershop/src/core/fp/either.dart';
+import 'package:dw_barbershop/src/core/fp/nil.dart';
+
+abstract interface class UserLoginService {
+
+  @override
+  Future<Either<ServiceException, Nil>> execute(String emal, String password);
+}
